3c7541e0b1ddbc8a362316d43d7cc996f39665e1
Alice Wang <aywang6@gmail.com>
2020-03-31 15:26:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -31,5 +31,9 @@ def find_valid_calls(planeteer_calls)
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above 
   # Return the first valid call found, or return nil if no valid calls are found
   
+  index = 0 
+  
+  while index < planeteer_calls.length do
+    planeteer_calls[index].detect 
   
 end
